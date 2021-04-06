343614f6ab8b5923b602e28b16ef51db0071d218
sujeong <tnwjd6496@naver.com>
2021-04-06 12:31:03 +0900

    [Programmers]수박수박수

---
+++ Programmers/연습문제/수박수박수.swift
@@ -0,0 +1,13 @@
+# 수박수박수박수박수박수?
+
+func solution(_ n:Int) -> String {
+    var answer = ""
+    for i in 0..<n {
+        if i % 2 == 0 {
+            answer += "수"
+        }else {
+            answer += "박"
+        }
+    }
+    return answer
+}
