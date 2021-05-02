03cc0f60048d1df6a7b6957541eaae21ca0c6ccd
sujeong <tnwjd6496@naver.com>
2021-05-02 10:37:43 +0900

    [프로그래머스]약수의 합

---
+++ Swift/Programmers/연습문제/약수의합.swift
@@ -0,0 +1,15 @@
+// 약수의 합
+
+func solution(_ n:Int) -> Int {
+    var answer = 0
+    guard n > 0 else {
+        return 0
+    }
+
+    for i in 1...n {
+        if n%i == 0 {
+            answer += i
+        }
+    }
+    return answer
+}
