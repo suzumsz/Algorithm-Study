408297fd1123fa30f43c1d9d0908fc607d4c3a61
sujeong <tnwjd6496@naver.com>
2021-05-22 21:54:15 +0900

    [프로그래머스] 두 개 뽑아서 더하기

---
+++ Swift/Programmers/연습문제/두개뽑아서더하기.swift
@@ -0,0 +1,13 @@
+// 두 개 뽑아서 더하기
+
+import Foundation
+
+func solution(_ numbers:[Int]) -> [Int] {
+    var answer = Set<Int>()
+    for i in 0..<numbers.count {
+        for j in i+1..<numbers.count{
+            answer.insert(numbers[i]+numbers[j])
+        }
+    }
+    return answer.sorted()
+}
